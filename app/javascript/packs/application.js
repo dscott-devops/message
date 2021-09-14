e1b72813be2fcab3422c3db56ec9ab0ca26bc4c4
Darryl Scott <dscott@rts-llc.com>
2021-09-14 15:04:20 +0000

    Added semantic ui

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,11 +2,17 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+// Loads all Semantic javascripts
+//= require jquery
+//= require semantic-ui
+//= require semantic-ui/modal
+//= require semantic-ui/dropdown
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import '@doabit/semantic-ui-sass/src/scss/semantic-ui.scss'
 
 Rails.start()
 Turbolinks.start()
